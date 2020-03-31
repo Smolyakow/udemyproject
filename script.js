0e86e5c36cdec9c20aeb2d93b4a641d2cac53da7
Yuriy <lemb44@gmail.com>
2020-03-31 16:55:43 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,23 @@
+// console.log(4/0);
+// console.log('string' / 2);
+
+
+// let person = {
+//     name: 'John',
+//     age: 25,
+//     isMarried: true
+// };
+
+// console.log(person.age);
+
+// let array = ['plum.png', 'orange.jpeg', 'apple.bmp'];
+
+// console.log(array[0]);
+
+console.log('mother' + 'hello');
+
+let isChecked = true,
+    isClose = !false;
+
+console.log(isChecked && isClose);
+
