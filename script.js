4434dbf94e296bf93eee5670a0a462d5237b2141
Yuriy <itlemb44@gmail.com>
2020-03-31 19:27:13 +0300

    first hw

--- script.js
+++ script.js
@@ -14,10 +14,24 @@
 
 // console.log(array[0]);
 
-console.log('mother' + 'hello');
+let money = +prompt ("Ваш бюджет на месяц?","");
+let time = prompt ("Введите дату в формате YYYY-MM-DD","");
 
-let isChecked = true,
-    isClose = false;
 
-console.log(isChecked && isClose);
+let appData = {
+    budget: money,
+    timeData: time,
+    expenses: {},
+    optionalExpenses: {},
+    income: [],
+    saving: false
+};
 
+const expensesArticle = prompt('Введите обязательную статью расходов в этом месяце', '');
+const expensesCost = prompt('Во сколько обойдется?', '');
+const expensesArticle2 = prompt('Введите обязательную статью расходов в этом месяце', '');
+const expensesCost2 = prompt('Во сколько обойдется?', '');
+appData.expenses[expensesArticle] = expensesCost;
+appData.expenses[expensesArticle2] = expensesCost2;
+
+alert(`Бюджет на 1 день: ${appData.budget / 30}`);
